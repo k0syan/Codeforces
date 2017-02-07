661b24a2d52cc0766f570730e7d26a51583f8037
shahenkosyan <shahen.qosyan@gmail.com>
2017-02-07 23:26:17 +0400

    Mahmoud and Longest Uncommon Subsequence

---
+++ 766A/mahmoud_and_longest_uncommon_ubsequence.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = input()
+    b = input()
+
+    if len(a) > len(b):
+        print(len(a))
+    elif len(b) > len(a):
+        print(len(b))
+    else:
+        if a == b:
+            print('-1')
+        else:
+            print(len(a))
