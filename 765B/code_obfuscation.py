be4a74caa96513989472f7f879a24944e4ffdba1
shahenkosyan <shahen.qosyan@gmail.com>
2017-02-17 22:52:42 +0400

    Code Obfuscation

---
+++ 765B/code_obfuscation.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    obfuscation = input()
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+
+    obfuscation = list(obfuscation)
+    letters = list(letters)
+
+    i = len(obfuscation)
+    posibility = 'NO'
+    while i:
+        if obfuscation[0] != letters[0]:
+            posibility = 'NO'
+            break
+        else:
+            while letters[0] in obfuscation:
+                obfuscation.remove(letters[0])
+                i -= 1
+                if len(obfuscation) == 0:
+                    posibility = 'YES'
+                    break
+            letters.remove(letters[0])
+
+    print(posibility)
