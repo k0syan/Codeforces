4a170cbfb39dd3dcbc6390857fa348777f73b4d6
shahenkosyan <shahen.qosyan@gmail.com>
2017-02-17 22:51:35 +0400

    Neverending competitions

---
+++ 765A/flights.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    n = int(input())
+    home_airport = input()
+    flights = []
+    while n > 0:
+        n -= 1
+        _from, _to = input().split('->')
+        flights.append(_from)
+        flights.append(_to)
+
+    count = 0
+
+    for i in range(len(flights)):
+        if flights[i] == home_airport:
+            count += 1
+
+    if count % 2:
+        print('contest')
+    else:
+        print('home')
